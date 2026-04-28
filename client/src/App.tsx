432c75be0371474672bce0686f0d80247d35d67e
Aztrec <38544464+MatheusOF@users.noreply.github.com>
2026-04-28 03:23:36 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home.tsx";
+import Home from "./pages/Home";
 
 function Router() {
   // make sure to consider if you need authentication for certain routes
